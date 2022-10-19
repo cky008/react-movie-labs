a442f6ff5a4a1f39d0c90361d8dd7e5db8c47f7d
Kaiyu Chen <fa2nica@Fa2niCadeMacBook-Pro.local>
2022-10-19 02:56:48 +0100

    E2E tests - Exercise - Navigating between Favourites page and movie details page.

--- movies/cypress/e2e/navigation.cy.js
+++ movies/cypress/e2e/navigation.cy.js
@@ -49,7 +49,16 @@ describe("Navigation", () => {
     );
   });
   describe("From the favourites page to a movie's details", () => {
-    // TODO
+    beforeEach(() => {
+        // Select two favourites and navigate to Favourites page
+        cy.get("button[aria-label='add to favorites']").eq(1).click();
+        cy.get("button[aria-label='add to favorites']").eq(3).click();
+        cy.get("button").contains("Favorites").click();
+      });
+    it("navigates to the movie details page and change browser URL from favourite page", () => {
+        cy.get(".MuiCardActions-root").eq(1).contains("More Info").click();
+        cy.url().should("include", `/movies/${movies[3].id}`);
+      });
   });
   describe("The forward/backward links", () => {
     beforeEach(() => {
